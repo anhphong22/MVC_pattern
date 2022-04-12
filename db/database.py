132bab0e74b864ff14917c33b1d63adf07595db5
phongnt <phongnt@ftech.ai>
2022-04-12 17:01:27 +0700

    update models

---
+++ db/database.py
@@ -0,0 +1,11 @@
+import sqlachemy as _sql
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE_URL = 'sqlite:///. db/database.db'
+engine = create_engine(DATABASE_URL, echo=True, connect_args={'check_same_thread': False})
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
+
+
